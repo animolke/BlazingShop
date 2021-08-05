7dc98584607eabd575b748a667c9b19f593e16aa
ANIKET MOLKE <animolke@gmail.com>
2021-08-06 02:11:01 +0530

    Add project files.

---
+++ BlazingShop/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using BlazingShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        void LoadProducts();
+    }
+}
